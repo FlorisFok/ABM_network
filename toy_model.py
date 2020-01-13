8343011b963e0c1084ccc10bca677ce36fd74ac8
jakob-ra <49194118+jakob-ra@users.noreply.github.com>
2020-01-13 08:41:17 +0100

    Add files via upload

--- toy_model.py
+++ toy_model.py
@@ -21,8 +21,8 @@ delta = 0.3 # weight placed on indirect links
 gamma = 0.5 # weight placed on additional utility derived from a mutual link
 c = 0.2 # cost of forming and maintaining links
 b = 0.3 # strength of preference for links to similar agents 
-sigma = 0 # standard deviation of the shocks to utility
-tau = 2.1 # convexity of costs
+sigma = 0.01 # standard deviation of the shocks to utility
+alpha = 2.1 # convexity of costs
 
 share_red = 1/3
 share_blue = 1/3
@@ -42,11 +42,10 @@ X = np.array([possible_X[0] for i in range(int(share_red*n))] +
               [possible_X[2] for i in range(n-int(share_red*n)-int(share_blue*n))])
 
 # Randomly generate the initial network configuration
-p_link_0 = 0 # Uniform initial link probability
+p_link_0 = 0.1 # Uniform initial link probability
 g_0 = np.random.choice([0, 1], size=(n,n), p=[1-p_link_0,p_link_0])
 np.fill_diagonal(g_0, 0) # The diagonal elements of the adjacency matrix are 0 by convention
 g_sequence = [g_0] # Sequence of adjacency matrices
-
 
 
 def u(i,j,X):
@@ -71,7 +70,7 @@ def U(i,g,X):
             else:
                 indirect_u += g[i,j]*g[j,k]*u(i,k,X)
             
-    return direct_u + gamma*mutual_u + delta*indirect_u - d_i**tau*c
+    return direct_u + gamma*mutual_u + delta*indirect_u - d_i**alpha*c
 
 def step(g,X):
     """ Randomly selects an agent i to revise their link with another random 
@@ -112,33 +111,45 @@ def plot_network(g):
     rows, cols = np.where(g == 1)
     edges = zip(rows.tolist(), cols.tolist())
     gr = nx.DiGraph() # Calling the DIRECTED graph method
+    gr.add_nodes_from(range(n))
     gr.add_edges_from(edges)
     # Add node colors according to X
     color_map = []
-    for i in gr.nodes:
-        if np.all(X[i]==[1,0]):
+    for i in range(n):
+        if np.all(X[i]==possible_X[0]):
             color_map.append('red')
-        if np.all(X[i]==[0,1]):
+        if np.all(X[i]==possible_X[1]):
             color_map.append('blue')
-        if np.all(X[i]==[1,1]):
+        if np.all(X[i]==possible_X[2]):
             color_map.append('green')
     nx.draw(gr, node_color=color_map, with_labels=True, node_size=500)
     
     plt.show()
 
 
-# Run the simulation for T total steps
-T=1000
-t_plot = 100 # Produce a plot and diagnostics every t_plot steps
+# Run the simulation for T total steps or until convergence is reached
+T = 5000
+t_plot = 100
+
 for t in range(T-1):
+    # Perform a step and attach the new network
     g_new = step(g_sequence[-1],X)
-    g_sequence.append(g_new)
-    if t%t_plot == 0 and t/t_plot>0:
-        plot_network(g_sequence[-1])
-#        print(np.linalg.norm((g_sequence[t+1]-g_sequence[t+1-t_plot]), ord=1))
+    g_sequence.append(g_new.copy())
+    
+    # Produce a plot and diagnostics every t_plot steps
+    if t%t_plot == 0:
+        plot_network(g_sequence[-2])
         degrees = [sum(g_sequence[-1][i]) for i in range(n)]
         print('Mean, variance of degree: {},{}'
               .format(np.mean(degrees),np.var(degrees)))
 #        print('Average degree for reds blues, and greens: {},{},{}'
 #              .format(np.mean([sum(g_sequence[-1][i]) for i in range(n)])))
+        
+     # Calculate how many links have changed and stop if convergence has been reached   
+        if t/t_plot > 0:
+            network_change = np.linalg.norm((g_sequence[-1]-g_sequence[-t_plot]), ord=1)
+            print(network_change)
+            if network_change == 0:
+                break
+
     
