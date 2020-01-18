49add3ebf8bf1700309182f3af82080f703aa83c
Koen Greuell <koengreuell@gmail.com>
2020-01-18 15:14:55 +0100

    Process all student features

--- read_moreno.py
+++ read_moreno.py
@@ -23,10 +23,10 @@ X_list = []
 
 for school in range(n_schools):
     url = 'http://moreno.ss.uci.edu/comm' + str(school+1)
-    
+
     # Read in network structure
     url_g = url + '.dat'
-    df_g = pd.read_csv(url_g, delim_whitespace=True, 
+    df_g = pd.read_csv(url_g, delim_whitespace=True,
                        names=['ego','alter','tie_strength'], skiprows=4)
     n = df_g.ego.max()
     g = np.zeros((n,n))
@@ -36,14 +36,14 @@ for school in range(n_schools):
         try:
             g[i,j] = 1
         except Exception as e:
-            print(e) # for infrequently encountered out of bound errors, i.e. 
+            print(e) # for infrequently encountered out of bound errors, i.e.
             # invalid nomination ids
-    
-    print(sum([sum(g[i]) for i in range(n)]/n)) # Check average degree to make 
+
+    print(sum([sum(g[i]) for i in range(n)]/n)) # Check average degree to make
     # sure everything is working as intended
-    
+
     g_list.append(g)
-    
+
 # Save list of g matrices as a pickle
 with open('g_list.pkl', 'wb') as f:
     pickle.dump(g_list, f)
@@ -51,24 +51,50 @@ with open('g_list.pkl', 'wb') as f:
 # To read later, use:
 #with open('g_list.pkl', 'rb') as f:
 #    g_list = pickle.load(f)
-    
-# Read in node features (this only works for the first file so far)
+
+# Read in node features
 for school in range(1,n_schools+1):
-    url = 'http://moreno.ss.uci.edu/comm' + str(school)
-    
-    url_X = url + '_att.dat'
-    df_X = pd.read_csv(url_X, delim_whitespace=True, names=['feature','value'], skiprows=8)
-    df_X = df_X.iloc[:-1]
-    df_X.reset_index(inplace=True)
-    df_X = df_X.astype('int32')
-    X = np.zeros((n,3))
-    for row in range(len(df_X)):
-        i = df_X['index'].iloc[row]-1
-        j = df_X.feature.iloc[row]-1
-        X[i,j] = df_X.value.iloc[row]
-    
+    # skip school 48 as it does not exist
+    if school != 48:
+        url = 'http://moreno.ss.uci.edu/comm' + str(school)
+        url_X = url + '_att.dat'
+
+        print("converting X data of school {}".format(school))
+
+        # do a different thing for the first school
+        if school == 1:
+            df_X = pd.read_csv(url_X, delim_whitespace=True, names=['feature', 'value'], skiprows=8)
+            df_X = df_X.iloc[:-1]
+            df_X.reset_index(inplace=True)
+            df_X = df_X.astype('int32')
+            X = np.zeros((n,3)) # you sure n is defined correctly? < Koen
+            for row in range(len(df_X)):
+                i = df_X['index'].iloc[row]-1
+                j = df_X.feature.iloc[row]-1
+                X[i,j] = df_X.value.iloc[row]
+        else:
+            df_X = pd.read_csv(url_X, delim_whitespace=True, names=['sex','race','grade','school'], skiprows=8)
+            X = np.array(df_X)
+            # remove top lines if they don't contain numbers < the number of rows before the data is not always equal
+            for i in range(3):
+                if not np.isfinite(X[0,2]):
+                    X = np.delete(X, obj=0, axis=0)
+
+            # change the value of the school_ID to 0 if it were nan
+            if not np.isfinite(X[0,3]):
+                X[:,3] = 0
+            # remove last row of school 35, as it contains a questionmark
+            if school == 35:
+                X = X[0:-1]
+            X = X.astype('int32')
+    else:
+        X = np.zeros((1,1))
+
     X_list.append(X)
-    
+
+# Save list of X matrices as a pickle
+with open('X_list.pkl', 'wb') as f:
+    pickle.dump(X_list, f)
 
 # Plot the generated networks
 def plot_network(g):
@@ -80,11 +106,11 @@ def plot_network(g):
     gr.add_edges_from(edges)
 
     nx.draw(gr, with_labels=False, node_size=100)
-    
+
     plt.show()
 
 for g in g_list:
     if len(g) < 5000:
         plot_network(g)
-        
+
 plot_network(g_list[0])
