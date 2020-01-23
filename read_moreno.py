ddbc1b6f7f4fad406c0c48f66e15b46946013fa3
jakob-ra <49194118+jakob-ra@users.noreply.github.com>
2020-01-23 17:45:24 +0100

    final version of script for reading the data
    - removes any person i with missing values in X_i
    - if X is longer than g by k rows, removes the last k rows from X
    - if X is shorter than g by k rows, removes the last k rows and k columns from g
    - gives out a list of the number of missing values for each community as well

--- read_moreno.py
+++ read_moreno.py
@@ -20,15 +20,46 @@ n_schools = 84
 
 g_list = []
 X_list = []
+missing_list = []
 
-for school in range(n_schools):
-    url = 'http://moreno.ss.uci.edu/comm' + str(school+1)
+# Read in node features
+for school in range(1,n_schools+1):
+    # skip school 1 and 48 as it does not exist/has weird data
+    if school == 1 or school == 48:
+        continue
+    
+    url = 'http://moreno.ss.uci.edu/comm' + str(school)
+    url_X = url + '_att.dat'
 
+#    print("converting data of school {}".format(school))
+
+    X = pd.read_csv(url_X, delim_whitespace=True, names=['sex','race','grade','school'], skiprows=8)
+    # remove top and bottom lines if they don't contain numbers < the number of rows before the data is not always equal
+    for i in range(5):
+        if np.isnan(X.grade.iloc[0]):
+            X = X.iloc[1:]
+        if np.isnan(X.grade.iloc[-1]):
+            X = X.iloc[:-1]
+        
+    n = len(X)
+    
+    # change the value of the school_ID to 0 if it were nan
+    X.school.fillna(0, inplace=True)
+    
+    # the last row of school 35 contains a "?", which should be converted into 0
+    if school == 35:
+        X.race.iloc[-1] = 0
+        
+    X = X.astype('int8')
+    
+    # give indices of observations with at least one missing value
+    missing_ind = np.unique(np.where(X[['sex','race','grade']] == 0)[0])
+    
     # Read in network structure
     url_g = url + '.dat'
     df_g = pd.read_csv(url_g, delim_whitespace=True,
                        names=['ego','alter','tie_strength'], skiprows=4)
-    n = df_g.ego.max()
+
     g = np.zeros((n,n))
     for row in range(len(df_g)):
         i = df_g.ego.iloc[row]-1
@@ -39,86 +70,103 @@ for school in range(n_schools):
             print(e) # for infrequently encountered out of bound errors, i.e.
             # invalid nomination ids
 
-    print(sum([sum(g[i]) for i in range(n)]/n)) # Check average degree to make
+#    print(sum([sum(g[i]) for i in range(n)]/n)) # Check average degree to make
     # sure everything is working as intended
-
-    g_list.append(g)
-
-# Save list of g matrices as a pickle
-with open('g_list.pkl', 'wb') as f:
-    pickle.dump(g_list, f)
-
-# To read later, use:
-#with open('g_list.pkl', 'rb') as f:
-#    g_list = pickle.load(f)
-
-# Read in node features
-for school in range(1,n_schools+1):
-    # skip school 48 as it does not exist
-    if school != 48:
-        #n = 71 # comment out if g_list is made
-        url = 'http://moreno.ss.uci.edu/comm' + str(school)
-        url_X = url + '_att.dat'
-
-        print("converting X data of school {}".format(school))
-
-        # do a different thing for the first school
-        if school == 1:
-            df_X = pd.read_csv(url_X, delim_whitespace=True, names=['feature', 'value'], skiprows=8)
-            df_X = df_X.iloc[:-1]
-            df_X.reset_index(inplace=True)
-            df_X = df_X.astype('int32')
-            X = np.zeros((n,3)) # you sure n is defined correctly? < Koen
-            for row in range(len(df_X)):
-                i = df_X['index'].iloc[row]-1
-                j = df_X.feature.iloc[row]-1
-                X[i,j] = df_X.value.iloc[row]
-        else:
-            df_X = pd.read_csv(url_X, delim_whitespace=True, names=['sex','race','grade','school'], skiprows=8)
-            X = np.array(df_X)
-            # remove top lines if they don't contain numbers < the number of rows before the data is not always equal
-            for i in range(3):
-                if not np.isfinite(X[0,2]):
-                    X = np.delete(X, obj=0, axis=0)
-
-            # change the value of the school_ID to 0 if it were nan
-            if not np.isfinite(X[0,3]):
-                X[:,3] = 0
-            # the last row of school 35 contains a "?", which should be converted into 0
-            if school == 35:
-                print('X[-1,1] = {}'.format(X[-1,1]))
-                X[-1,1] = 0
-            X = X.astype('int32')
-
-        # convert the race data to one variable per race
-        race_numbers = X[:,1]
-        dummy_race = pd.get_dummies(race_numbers)
-        X = np.concatenate((X, dummy_race), axis=1)
-
-    else:
-        X = np.zeros((1,1))
-
+    
+    # Remove missing values 
+    X = X[~X.index.isin(missing_ind)]
+    
+    # Remove rows and columns with index in missing_values
+    for i in missing_ind:
+        try:
+            g = np.delete(g, i, axis=0)
+            g = np.delete(g, i, axis=1)
+        except Exception as e:
+            print(e) # out of bound errors
+    
+    # If length of X and g are not equal, remove last obs from the longer dataset
+    g_X_diff = len(g)-len(X)
+    print(g_X_diff)
+    if g_X_diff > 0:
+        g = g[:-abs(g_X_diff),:-abs(g_X_diff)]
+    if g_X_diff < 0:
+        X = X[:-abs(g_X_diff)]
+        
+    g = g.astype('int8')
+    
+    print('Community {}: {} observations and  {} missing'.format(school,
+          len(X), n-len(X)))
+  
     X_list.append(X)
+    g_list.append(g)
+    missing_list.append(n-len(X))
+    
+print('{} is the average share of missing observations.'.format(
+        np.average([missing_list[i]/len(X_list[i]) for i in range(len(missing_list))])))
+
+#for school in range(len(X_list)):
+#    print(sum([X_list[school].iloc[i]['sex','race','grade']==0 for i in range(len(X_list[school]))]))
+#    print(range(len(X_list[school])),'\n')
+#    
 
 # Save list of X matrices as a pickle
 with open('X_list.pkl', 'wb') as f:
     pickle.dump(X_list, f)
+   
 
-# Plot the generated networks
-def plot_network(g):
-    """ Uses networkX to plot the directed network g """
-    rows, cols = np.where(g == 1)
-    edges = zip(rows.tolist(), cols.tolist())
-    gr = nx.DiGraph() # Calling the DIRECTED graph method
-    gr.add_nodes_from(range(n))
-    gr.add_edges_from(edges)
+# Save list of g matrices as a pickle
+with open('g_list.pkl', 'wb') as f:
+    pickle.dump(g_list, f)  
+    
+# Save list of missing as a pickle
+with open('missing_list.pkl', 'wb') as f:
+    pickle.dump(missing_list, f) 
+    
+# Descriptive statistics
+#for i in range(len(X_list)):
+#    if len(X_list[i])<100:
+#        print('\nSchool {}'.format(i))
+#        print(X_list[i].describe())
+#
+#small_sample_ind = [67,68,74]
+#X_small_sample = [X_list[i] for i in small_sample_ind]
+#g_small_sample = [g_list[i] for i in small_sample_ind]
+#
+#with open('g_sample_list.pkl', 'wb') as f:
+#    pickle.dump(g_small_sample, f)
+#
+#with open('X_sample_list.pkl', 'wb') as f:
+#    pickle.dump(X_small_sample, f)    
+#    
+## To read later, use:
+#with open('g_list.pkl', 'rb') as f:
+#    g_list = pickle.load(f)
+#
+#
+## Plot the generated networks
+#def plot_network(g, color_map):
+#    """ Uses networkX to plot the directed network g """
+#    n = len(g)
+#    rows, cols = np.where(g == 1)
+#    edges = zip(rows.tolist(), cols.tolist())
+#    gr = nx.DiGraph() # Calling the DIRECTED graph method
+#    gr.add_nodes_from(range(n))
+#    gr.add_edges_from(edges)
+#
+#    nx.draw(gr, node_color=color_map, with_labels=False, node_size=50)
+#
+#    plt.show()
+#
+#
+#for comm in range(len(small_sample_ind)):
+#    # Add node colors according to X
+#    color_map = []
+#    for i in range(len(g_small_sample[comm])):
+#        if X_small_sample[comm].sex.iloc[i] == 1:
+#            color_map.append('red')
+#        if X_small_sample[comm].sex.iloc[i] == 2:
+#            color_map.append('blue')
+#
+#    plot_network(g_small_sample[comm], color_map)
 
-    nx.draw(gr, with_labels=False, node_size=100)
 
-    plt.show()
-
-for g in g_list:
-    if len(g) < 5000:
-        plot_network(g)
-
-plot_network(g_list[0])
