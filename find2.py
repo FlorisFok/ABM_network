344f570f3edf8d14f48d52c775d1819519b69f8e
florisfok <florisfok5@gmail.com>
2020-01-29 08:23:19 +0100

    SA

--- find2.py
+++ find2.py
@@ -9,7 +9,6 @@ import copy
 import os
 import pickle
 
-fig, (ax1, ax2, ax3) = plt.subplots(1,3)
 colors = ['#FF0000', '#00FF00', '#0000FF',
           '#FFFF00', '#00FFFF', '#FF00FF', '#C0C0C0',
           '#808080', '#800000',	'#808000', '#008000',
@@ -32,20 +31,32 @@ def stop_rule(zero_sequence, t) :
     return zero_sequence[t - n_zeros :t].any() == zeros.any()
 
 
-def analyse_network(connectivity) :
-    # Output:
-    # density at maximum reach (RCHDEN), > den_max_reach
-    # relative density (RELDEN), > rel_den
-    # proportion symmetric dyads (PTCMUT), > p_symm_dyads
-    # mutuality index (RHO2), > mutuality_index
+def analyse_network(connectivity, characteristics) :
+    # OUTPUT:
+    #
+    # average degree of nodes network            > av_degree
+    # density at maximum reach (RCHDEN)          > den_max_reach
+    # relative density (RELDEN)                  > rel_den
+    # proportion symmetric dyads (PTCMUT)        > p_symm_dyads
+    # mutuality index (RHO2)                     > mutuality_index
+    # amount of clustering (groups of friends)   > clustering_coefficient   [mean, std dev]
+    # homophily index per trait in dict          > homoph_ind               {"sex_", "race_", "grade_"}
+    # segregation index per trait in tuple       > segreg_ind               [sex, race, grade]
 
-    # input:
-    # connectivity matrix with students claiming to have friends in row and students claimed to be befriended in columns
+    # INPUT:
+    # - connectivity matrix with row-students nominating column-students as friends
+    # - characteristics matrix with row per student, with integers indicating every group for each characteristic (sex, race, grade)
+    charr = np.zeros((characteristics.shape[0], 3))
+
+    for i in range(3) :
+        charr[:, i] = characteristics.iloc[:, i]
+
+    characteristics = charr
 
     nodes = connectivity.shape[0]
     mutual_d = 0
     asym_d = 0
-    out_degrees = []
+    trait = ["sex", "race", "grade"]
 
     # density DENX2
     # DENX2 = np.sum(connectivity)/(nodes*(nodes-1))
@@ -98,6 +109,8 @@ def analyse_network(connectivity) :
 
     # count total out_degree connections
     out_degree = connectivity.sum()
+    # average out_degree
+    av_degree = out_degree / nodes
     # take the sum of squares of the out degree connections per individual (row)
     sum_squares_out = (connectivity.sum(axis=1) ** 2).sum()
 
@@ -122,7 +135,168 @@ def analyse_network(connectivity) :
     std_clustering_coefficient = np.std(clustering_coefficients)
     clustering_coefficient = [mean_clustering_coefficient, std_clustering_coefficient]
 
-    return RCHDEN, RELDEN, RHO2, clustering_coefficient[0]
+    # create homophily index dictionary and lists for the segregation index
+    homoph_ind = dict()
+    segreg_ind = []
+
+    # iterate through different characteristics (sex, race, grade)
+    for i in range(characteristics.shape[1]) :
+        # get different groups of this characteristic in dataset
+        characs = sorted(list(set(characteristics[:, i])))
+        amount = len(characs)
+
+        # counters for individuals per characteristic group and out-group nominations
+        charac_count = []
+        charac_out = []
+
+        # counters for expected and observed cross trait nominations
+        exp_cross = 0
+        obs_cross = 0
+
+        # iterate through different groups of this characteristic
+        for j in range(amount) :
+            # indicate indices of members this group and save size group
+            indices = np.where(characteristics[:, i] == characs[j])[0]
+            charac_count.append(len(indices))
+
+            # create a submatrix of all nominations from this group and save amount
+            submat_trait = connectivity[np.ix_(indices, )]
+            charac_out.append(submat_trait.sum())
+
+            # create 2 submatrices outgoing connections: 1 to individuals same group and 1 to individuals different group
+            submat_same = connectivity[np.ix_(indices, indices)]
+            mask = np.ones(connectivity.shape[0], np.bool)
+            mask[indices] = 0
+            submat_diff = submat_trait[:, mask]
+
+            # count amount outgoing connections to same and to different group
+            out_same = np.mean(submat_same.sum(axis=1))
+            out_diff = np.mean(submat_diff.sum(axis=1))
+
+            # add amount of cross trait nominations of this group to total
+            obs_cross += submat_diff.sum()
+
+            # calculate and save homophily index from this group for this characteristic
+            homoph_ind[str(trait[i]) + "_" + str(characs[j])] = out_same / (out_same + out_diff)
+
+        # calculate expected cross trait nominations (added for all combinations of traits (except with self))
+        for row in range(amount) :
+            for col in range(amount) :
+                if row == col :
+                    pass
+                else :
+                    exp_cross += charac_out[row] * (
+                                (charac_count[row] * charac_count[col]) / (charac_count[row] * (nodes - 1)))
+
+        # calculate and save segregation index for this trait
+        if exp_cross == 0 :
+            segreg_ind.append(9999)
+            if amount == 1 :
+                warnings.warn("Just one group: no segregation index for trait " + str(trait[i]) + "possible.")
+            else :
+                warnings.warn("Expected cross trait nominations is zero: no segregation index for trait " + str(
+                    trait[i]) + "possible.")
+        else :
+            segreg_ind.append((exp_cross - obs_cross) / exp_cross)
+
+    # return av_degree, RCHDEN, RELDEN, RHO2, segreg_ind[0], segreg_ind[1], segreg_ind[2]
+    if clustering_coefficient[0] != clustering_coefficient[0]:
+        clustering_coefficient[0] = 0
+
+    return segreg_ind[0], segreg_ind[1], segreg_ind[2]
+
+#
+# def analyse_network(connectivity) :
+#     # Output:
+#     # density at maximum reach (RCHDEN), > den_max_reach
+#     # relative density (RELDEN), > rel_den
+#     # proportion symmetric dyads (PTCMUT), > p_symm_dyads
+#     # mutuality index (RHO2), > mutuality_index
+#
+#     # input:
+#     # connectivity matrix with students claiming to have friends in row and students claimed to be befriended in columns
+#
+#     nodes = connectivity.shape[0]
+#     mutual_d = 0
+#     asym_d = 0
+#     out_degrees = []
+#
+#     # density DENX2
+#     # DENX2 = np.sum(connectivity)/(nodes*(nodes-1))
+#
+#     # density at maximum reach RCHDEN
+#
+#     # define the function to tranfer adjacency matrix to reachability matrix
+#     # Prints reachability matrix of graph[][] using Floyd Warshall algorithm
+#     # function found on https://www.geeksforgeeks.org/transitive-closure-of-a-graph/
+#     reachability = copy.deepcopy(connectivity)
+#     '''reach[][] will be the output matrix that will finally
+#     have reachability values.
+#     Initialize the solution matrix same as input graph matrix'''
+#     reach = [i[:] for i in reachability]
+#     '''Add all vertices one by one to the set of intermediate
+#     vertices.
+#     ---> Before start of a iteration, we have reachability value
+#     for all pairs of vertices such that the reachability values
+#     consider only the vertices in set
+#     {0, 1, 2, .. k-1} as intermediate vertices.
+#     ----> After the end of an iteration, vertex no. k is
+#     added to the set of intermediate vertices and the
+#     set becomes {0, 1, 2, .. k}'''
+#     for k in range(nodes) :
+#
+#         # Pick all vertices as source one by one
+#         for i in range(nodes) :
+#
+#             # Pick all vertices as destination for the
+#             # above picked source
+#             for j in range(nodes) :
+#                 # If vertex k is on a path from i to j,
+#                 # then make sure that the value of reach[i][j] is 1
+#                 reach[i][j] = reach[i][j] or (reach[i][k] and reach[k][j])
+#
+#     RCHDEN = np.sum(reach) / (nodes * (nodes - 1))
+#
+#     # relative density RELDEN
+#     RELDEN = np.sum(connectivity) / (10 * nodes)
+#
+#     # create upper triangular matrix with 2's on mutual dyads, 1's on asymmetric dyads and count occurrence
+#     added_up = np.triu(connectivity + np.transpose(connectivity))
+#     mutual_d = np.count_nonzero(added_up == 2)
+#     asym_d = np.count_nonzero(added_up == 1)
+#     total_d = mutual_d + asym_d
+#
+#     # calculate proportion symmetric dyads (PTCMUT) and asymmetric dyads (PTCASY)
+#     PTCMUT = mutual_d / total_d
+#     # PTCASY = asym_d / total_d
+#
+#     # count total out_degree connections
+#     out_degree = connectivity.sum()
+#     # take the sum of squares of the out degree connections per individual (row)
+#     sum_squares_out = (connectivity.sum(axis=1) ** 2).sum()
+#
+#     # calculate mutuality index (RHO2) (according to Katz and Powell’s (1955))
+#     RHO2 = (2 * (nodes - 1) ** 2 * mutual_d - out_degree ** 2 + sum_squares_out) / (
+#                 out_degree * (nodes - 1) ** 2 - out_degree ** 2 + sum_squares_out)
+#
+#     # determine the local clustering coefficient mean and standard deviation
+#     clustering_coefficients = []
+#     for n_node, connections in enumerate(connectivity) :
+#         # the amount of neighbours each node has
+#         n_neighbours = np.sum(connectivity[n_node])
+#         # only consider nodes with at least 2 neighbours
+#         if n_neighbours >= 2 :
+#             # matrix of the nodes that are both neighbours of the node considered
+#             neighbour_matrix = np.dot(np.transpose([connectivity[n_node]]), [connectivity[n_node]])
+#             # the amount of connections between neighbours
+#             neighbour_connections = np.sum(connectivity * neighbour_matrix)
+#             # the amount of connections between neighbours divided by the possible amount of connections
+#             clustering_coefficients.append(neighbour_connections / (n_neighbours * (n_neighbours - 1)))
+#     mean_clustering_coefficient = np.mean(clustering_coefficients)
+#     std_clustering_coefficient = np.std(clustering_coefficients)
+#     clustering_coefficient = [mean_clustering_coefficient, std_clustering_coefficient]
+#
+#     return RCHDEN, RELDEN, RHO2, clustering_coefficient[0]
 
 
 class ConnectionMatrix:
@@ -320,13 +494,12 @@ class Model:
         for t in range(1, total_time):
             # Perform a step and attach the new network
             self.step()
-            print('step:', t, end='\r')
+            # print('step:', t, end='\r')
 
             self.g_sequence[t] = self.g.g
             self.zero_sequence[t] = conv_rule(self.g_sequence, t)
 
             if t > MINIMAL and stop_rule(self.zero_sequence, t):
-                print('STOPPED at', t)
                 break
 
             # Produce a plot and diagnostics every t_plot steps
@@ -413,97 +586,76 @@ def read_excel_settings(loc):
 def avg(l, reruns):
     return [sum(l[i*reruns:(i+1)*reruns])/reruns for i in range(int(len(l)/reruns))]
 
-if __name__ == "__main__":
-    # Settings
-    name = [i for i in os.listdir() if 'xlsx' in i and 'settings' in i][0]
-    settings = read_excel_settings(name)
+def main(settings, repeats):
+    global DELTA, GAMMA, C, B1, B2, B3, SIGMA, ALPHA, MIN_PROP, pos_link, MINIMAL, MAX_FRIEND, zeros, n_zeros
+    B1, B2, B3 = settings
+    DELTA, GAMMA, C, SIGMA = [0.05000000000000004, 0.6499999999999999, 0.175, 0.035]
+    #[0.12500000000000003, 0.6499999999999999, 0.19999999999999998, 0.01]
 
     # Plot info
-    plot_data = {'RCHDEN':[], 'RELDEN':[], 'RHO2':[], 'clustering_coefficient':[], 'stopped':[]}
-    plot_var = 'min prop'
-    _string = ['RCHDEN', 'RELDEN', 'RHO2', 'clustering_coefficient']
+    # plot_data = {'av_degree':[], 'RCHDEN':[], 'RELDEN':[], 'RHO2':[],
+    #              'segreg_ind0':[], 'segreg_ind1':[], 'segreg_ind2':[]}
+    plot_data = {'segreg_ind0':[], 'segreg_ind1':[], 'segreg_ind2':[]}
+
+    # _string = ['av_degree', 'RCHDEN', 'RELDEN', 'RHO2', 'segreg_ind0', 'segreg_ind1', 'segreg_ind2']
+    _string = ['segreg_ind0', 'segreg_ind1', 'segreg_ind2']
 
     # True
     g_matrix = pickle.load(open(r"C:\Users\FlorisFok\Downloads\g_list.pkl", 'rb'))
     big_x = pickle.load(open(r"C:\Users\FlorisFok\Downloads\x_list.pkl", 'rb'))
+
     MAX_LEN = 100
     MAX_FRIEND = 5
+    MIN_PROP = 9
+    pos_link = 0.1
+    runs = 1000
+    ALPHA = 2
 
-    for row in range(len(settings[plot_var])):
-        # CONSTANTS
-        DELTA = settings['Delta'][row]  # weight placed on indirect links
-        GAMMA = settings['Gamma'][row]  # weight placed on additional utility derived from a mutual link
-        C = settings['C'][row]  # cost of forming and maintaining links
-        B1,B2, B3, = settings['B1'][row], settings['B2'][row], settings['B3'][row]  # strength of preference for links_s
-        SIGMA = settings['Sigma'][row]  # standard deviation of the shocks to utility
-        ALPHA = settings['Alpha'][row]  # convexity of costs
-        MIN_PROP = settings['min prop'][row]
-        pos_link = settings['pos link'][row]
+    n_zeros = 3
+    zeros = np.zeros(n_zeros)
+    MINIMAL = n_zeros
+    ########################
 
-        n_zeros = int(settings['n zeros'][row])
-        zeros = np.zeros(n_zeros)
-        MINIMAL = n_zeros
-        ########################
-
-        for rerun in [6, 7]:
-            X = big_x[rerun]
+    models = []
+    for model_num in range(len(big_x)):
+        for rerun in range(repeats):
+            X = big_x[model_num]
             possible_X = [i[0] for i in list(X.groupby(['sex', 'race']))]
             n_agents = len(X['sex'])
 
-            # if n_agents > MAX_LEN:
-            #     continue
+            if n_agents > MAX_LEN:
+                continue
 
             # Make and run model
             g = ConnectionMatrix(n_agents, pos_link)
             M = Model(g, n_agents, X, possible_X)
-            stopped = M.run(int(settings['runs'][row]), 0)
-            M.rank()
+            M.run(runs, 0)
+            # M.rank()
 
-            output = analyse_network(M.g.g)
+            output = analyse_network(M.g.g, X)
+            models.append(model_num)
             for s, o in zip(_string, output):
                 plot_data[s].append(o)
 
-            plot_data['stopped'].append(stopped)
+    # true_data = {'av_degree':[], 'RCHDEN':[], 'RELDEN':[], 'RHO2':[],
+    #              'segreg_ind0':[], 'segreg_ind1':[], 'segreg_ind2':[]}
+    true_data = {'segreg_ind0':[], 'segreg_ind1':[], 'segreg_ind2':[]}
 
-            M.plot_network()
-
-
-    true_data = {'RCHDEN':[], 'RELDEN':[], 'RHO2':[], 'clustering_coefficient':[], 'stopped':[]}
-    for g_num in [6, 7]:
-        g = g_matrix[g_num]
-        if g.shape[0] > MAX_LEN:
-            continue
-
-        output2 = analyse_network(g)
+    for num in models:
+        g = g_matrix[num]
+        X = big_x[num]
+        output2 = analyse_network(g, X)
         for s, o in zip(_string, output2):
             true_data[s].append(o)
 
 
+    best = 0
+    for t in _string:
+        mv = avg(plot_data[t], repeats)
+        for tv, v in zip(true_data[t], mv):
+            best += abs(tv - v)
 
-    pickle.dump(true_data, open('true_data.p', 'wb'))
-    pickle.dump(plot_data, open('plot_data.p', 'wb'))
+    return best
 
-
-    plt.title(f'{plot_var} vs. Analyse')
-    plt.subplot(121)
-    repeats = int(len(plot_data['stopped'])/len(settings[plot_var]))
-
-    plt.plot(settings[plot_var], avg(plot_data[_string[0]], repeats), label='RCHDEN', color='r')
-    plt.plot(settings[plot_var], avg(plot_data[_string[1]], repeats), label='RELDEN', color='g')
-    plt.plot(settings[plot_var], avg(plot_data[_string[2]], repeats), label='RHO2', color='y')
-    plt.plot(settings[plot_var], avg(plot_data[_string[3]], repeats), label='clustering_coefficient', color='k')
-
-    le = len(settings[plot_var])
-    plt.plot(settings[plot_var], avg(true_data[_string[0]], repeats)*le, linestyle='--', color='r')
-    plt.plot(settings[plot_var], avg(true_data[_string[1]], repeats)*le, linestyle='--', color='g')
-    plt.plot(settings[plot_var], avg(true_data[_string[2]], repeats)*le, linestyle='--', color='y')
-    plt.plot(settings[plot_var], avg(true_data[_string[3]], repeats)*le, linestyle='--', color='k')
-
-    plt.xlabel(plot_var)
-    plt.legend()
-
-    plt.subplot(122)
-    plt.plot(settings[plot_var], avg(plot_data['stopped'], repeats), label='stopped')
-    plt.xlabel(plot_var)
-    plt.legend()
-    plt.show()
+if __name__ == "__main__":
+    pass
